ac53e9cba030eba3fd1042ce6880f6c33788eeec
giv3x <giv3xconnect@gmail.com>
2025-02-11 13:18:51 +0700

    api for auth

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -12,3 +12,23 @@ export async function getProducts() {
         return null;
     }
 }
+
+export async function registerUser(userData) {
+    try{
+        const response = await fetch(`${API_BASE_URL}signup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Ошибка: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Ошибка при регистрации:", error);
+        return null;
+    }
+}
